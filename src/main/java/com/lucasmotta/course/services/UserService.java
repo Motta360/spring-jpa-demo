e68a8bd3ec2ce7172f3466d5ae6d3f9231b30745
Motta360 <lucas-mottateixeira@hotmail.com>
2024-05-24 09:30:59 -0300

    Exception handling - findById

--- src/main/java/com/lucasmotta/course/services/UserService.java
+++ src/main/java/com/lucasmotta/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.lucasmotta.course.entities.User;
 import com.lucasmotta.course.repositories.UserRepository;
+import com.lucasmotta.course.services.exception.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
 	}
 
