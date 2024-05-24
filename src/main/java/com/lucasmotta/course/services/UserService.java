bd3991cd00b3e44007fd8ece1c76cd36e4bb5e89
Motta360 <lucas-mottateixeira@hotmail.com>
2024-05-24 09:13:41 -0300

    User Update

--- src/main/java/com/lucasmotta/course/services/UserService.java
+++ src/main/java/com/lucasmotta/course/services/UserService.java
@@ -8,26 +8,39 @@ import org.springframework.stereotype.Service;
 
 import com.lucasmotta.course.entities.User;
 import com.lucasmotta.course.repositories.UserRepository;
+
 @Service
 public class UserService {
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
-		
+
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());	
 	}
 }
