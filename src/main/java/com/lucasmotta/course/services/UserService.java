cf29913b51fc83728f3cf74b5c16ba6c5d1fd225
Motta360 <lucas-mottateixeira@hotmail.com>
2024-05-27 08:42:28 -0300

    Exception handling - update

--- src/main/java/com/lucasmotta/course/services/UserService.java
+++ src/main/java/com/lucasmotta/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.lucasmotta.course.repositories.UserRepository;
 import com.lucasmotta.course.services.exception.DatabaseException;
 import com.lucasmotta.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -37,20 +39,24 @@ public class UserService {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());	
+		entity.setPhone(obj.getPhone());
 	}
 }
